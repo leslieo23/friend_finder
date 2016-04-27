b9868d53ad955c45533ed6c3f5e4ed6e9191fbf8
leslieo23 <leslie.obour23@gmail.com>
2016-04-27 11:34:55 -0400

    setting routes

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ var path= require('path');
 
 var app= express();
 var PORT= process.env.PORT||3001;
+var staticContentFolder;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+require('.app/routing/html-routes.js')(app);
+require('.app/routing/api-routes.js')(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
