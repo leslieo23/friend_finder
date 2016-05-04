dff2160df6146515ebc4cfc055bce9eb3daf0706
leslieo23 <leslie.obour23@gmail.com>
2016-05-04 15:00:11 -0400

    working on html and api routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -4,6 +4,9 @@ module.exports=function(app){
 	app.get('/survey',function(req,res){
 		res.sendFile(path.join(__dirname+'/../public/survey.html'))
 	});
+	app.get('/api/friends',function(req,res){
+		res.sendFile('/../data/friends.js')
+	});
 	app.use(function(req,res){
 		res.sendFile(path.join(__dirname+'/../public/home.html'))
 	});
